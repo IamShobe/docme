177408499bf21fcab3a2d10f61c3af3e6063e5cd
Elran Shefer <elran777@gmail.com>
2018-12-29 20:13:46 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ setup(
     name='docme',
     version=__version__,
     description="Auto documentation generating",
-    # long_description=open("README.rst").read(),
+    long_description=open("README.rst").read(),
     license="MIT",
     author="Elran Shefer",
     author_email="elran777@gmail.com",
     keywords="doc",
     install_requires=requirements,
-    python_requires="~=2.7.0",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
     entry_points={
         "console_scripts": [
             "docme = docme.cli:main"
@@ -30,7 +30,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Testing',
         'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
         'Operating System :: Unix',
